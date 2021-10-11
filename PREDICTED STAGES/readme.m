855f973f8fc1caa322c201bab7200d933d5b52a1
Ibidamola Olley <73557023+olleyibi@users.noreply.github.com>
2021-10-11 12:30:44 +1100

    Add files via upload

---
+++ PREDICTED STAGES/readme.m
@@ -0,0 +1,8 @@
+Run the 1 new_data.R file first
+Then run any other file changing the vegetation index and predictor (min_dv, max_dv or *maxndvi*) to get a desired output
+
+This files are used to predict the 
+           Anthesis stage
+           Harvest stage
+           Pinnacle Initaition Stage
+           Permanent Water Stage
